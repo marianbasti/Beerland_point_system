778c2126bd5d6864c4ef471a42dda4ef72be8bab
marixer <marixer@users.noreply.github.com>
2019-08-25 22:00:30 -0300

    Update db.sql

--- db.sql
+++ db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 18, 2019 at 04:57 PM
+-- Generation Time: Aug 26, 2019 at 02:59 AM
 -- Server version: 10.1.38-MariaDB
 -- PHP Version: 7.3.2
 
@@ -34,15 +34,27 @@ CREATE TABLE `clientes` (
   `nacimiento` date NOT NULL,
   `localidad` char(255) NOT NULL,
   `sexo` set('m','f','o') NOT NULL,
-  `tarjeta` int(11) NOT NULL
+  `tarjeta` char(35) NOT NULL,
+  `puntos` int(11) NOT NULL,
+  `id_app` int(11) DEFAULT NULL,
+  `rubia` int(11) NOT NULL,
+  `negra` int(11) NOT NULL,
+  `roja` int(11) NOT NULL,
+  `temporada` int(11) NOT NULL,
+  `otroalcohol` int(11) NOT NULL,
+  `salcohol` int(11) NOT NULL,
+  `comida` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `clientes`
 --
 
-INSERT INTO `clientes` (`id`, `documento`, `nacimiento`, `localidad`, `sexo`, `tarjeta`) VALUES
-(3, 39584399, '1996-05-06', 'Temperley', 'm', 123456789);
+INSERT INTO `clientes` (`id`, `documento`, `nacimiento`, `localidad`, `sexo`, `tarjeta`, `puntos`, `id_app`, `rubia`, `negra`, `roja`, `temporada`, `otroalcohol`, `salcohol`, `comida`) VALUES
+(3, 39584399, '1996-05-06', 'Temperley', 'm', '123456789', 4730, NULL, 9, 10, 33, 2, 0, 14, 22),
+(8, 39584395, '2007-06-13', 'Venado Tuerto', 'f', '123456785', 0, NULL, 0, 0, 0, 0, 0, 0, 0),
+(10, 395848484, '1999-02-03', 'Temperley', 'm', '987654321', 31285, NULL, 10, 139, 9, 12, 0, 0, 58),
+(12, 395888643, '2019-07-30', 'El Cóndor', 'f', 'ñ6061268028901086¡200210100000_', 7240, NULL, 2, 17, 6, 0, 0, 18, 31);
 
 --
 -- Indexes for dumped tables
@@ -62,7 +74,7 @@ ALTER TABLE `clientes`
 -- AUTO_INCREMENT for table `clientes`
 --
 ALTER TABLE `clientes`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
